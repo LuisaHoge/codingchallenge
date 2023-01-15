3ceca7cdf5af35bd73a6ff44f6ec3f226a5c5108
Luisa <luisahoge@gmail.com>
2023-01-15 21:04:10 +0100

    update top artist ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService} from '../app/Services/api.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 export interface Artist {
   name: string;
@@ -16,6 +17,14 @@ export interface Track {
   rank: number;
   url: string;
 }
+
+export interface Album {
+  name: string;
+  playcount: number;
+  rank: number;
+  url: string;
+}
+
 
 export interface TopArtistsInCountry{
   rank: number;
@@ -62,6 +71,14 @@ export class AppComponent {
     { rank: 5, name: '', playcount: 0},
   ];
 
+  albums: Album[] = [
+    { name: '', url: '', rank: 1, playcount: 0 },
+    { name: '', url: '', rank: 2, playcount: 0 },
+    { name: '', url: '', rank: 3, playcount: 0 },
+    { name: '', url: '', rank: 4, playcount: 0 },
+    { name: '', url: '', rank: 5, playcount: 0 },
+  ];
+
   constructor(private apiService: ApiService) { }
 
   searchMusic(artist: string) {
@@ -91,6 +108,24 @@ export class AppComponent {
         this.tracks[4].name = res.toptracks.track[4].name;
         this.tracks[4].url = res.toptracks.track[4].url;
         this.tracks[4].listeners = res.toptracks.track[4].listeners;
+    });
+
+    this.apiService.searchMusic(artist, 'gettopalbums').subscribe((res: any) => {
+      this.albums[0].name = res.topalbums.album[0].name;
+      this.albums[0].url = res.topalbums.album[0].url;
+      this.albums[0].playcount = res.topalbums.album[0].playcount;
+      this.albums[1].name = res.topalbums.album[1].name;
+      this.albums[1].url = res.topalbums.album[1].url;
+      this.albums[1].playcount = res.topalbums.album[1].playcount;
+      this.albums[2].name = res.topalbums.album[2].name;
+      this.albums[2].url = res.topalbums.album[2].url;
+      this.albums[2].playcount = res.topalbums.album[2].playcount;
+      this.albums[3].name = res.topalbums.album[3].name;
+      this.albums[3].url = res.topalbums.album[3].url;
+      this.albums[3].playcount = res.topalbums.album[3].playcount;
+      this.albums[4].name = res.topalbums.album[4].name;
+      this.albums[4].url = res.topalbums.album[4].url;
+      this.albums[4].playcount = res.topalbums.album[4].playcount;
     });
   }
 

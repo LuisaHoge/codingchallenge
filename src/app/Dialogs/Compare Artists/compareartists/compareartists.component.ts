472b2fdc4fc459c8fc54c1517cb9548b0e4552c9
Luisa <luisahoge@gmail.com>
2023-01-18 19:04:04 +0100

    compare artist update

--- src/app/Dialogs/Compare Artists/compareartists/compareartists.component.ts
+++ src/app/Dialogs/Compare Artists/compareartists/compareartists.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { DialogData, Track } from 'src/app/app.component';
+import { ApiService } from 'src/app/Services/api.service';
 
 
 @Component({
@@ -13,7 +14,21 @@ export class CompareartistsComponent {
   constructor(
     public dialogRef: MatDialogRef<CompareartistsComponent>,
    // @Inject(MAT_DIALOG_DATA) public data: DialogData,
-  ) {}
+private apiService: ApiService) {}
+
+  searchStr: string = "";
+
+  searchMusic(artist: string) {
+    console.log(artist);
+    /*this.apiService.searchMusic(artist, 'getinfo').subscribe((res: any) => {
+      this.artist.name = res.artist.name;
+      this.artist.image = res.artist.image[2]['#text'];
+      this.artist.listeners = res.artist.stats.listeners;
+      this.artist.playcount = res.artist.stats.playcount;
+      this.artist.summary = res.artist.bio.summary;
+      this.artist.url = res.artist.url;
+    });*/
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
